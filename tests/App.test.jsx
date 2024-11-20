3ad614fd8fbbafb5acf61edfdd166cb5e3b03e3a
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2024-11-20 10:33:42 +0100

    Added favicon

--- tests/App.test.jsx
+++
@@ -1,6 +0,0 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "../src/App";
-import userEvent from "@testing-library/user-event";
-
-describe("App component", () => {});
